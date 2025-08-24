af296190136020d22dd0bdfcdbc36108253cce4a
Thao Crouch <thaocrouch@gmail.com>
2025-08-24 23:48:01 +0700

    update notify - send email, push notify to internal system

--- src/PaymentGateway.Api/Helpers/CommonHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace PaymentGateway.Api.Helpers;
-
-public static class CommonHelper
-{
-    public static string ToJson(this object obj)
-    {
-        return JsonConvert.SerializeObject(obj);
-    }
-}
